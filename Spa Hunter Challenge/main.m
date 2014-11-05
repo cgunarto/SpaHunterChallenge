c9b2426403c31f0e36f3f8e6402078047a62f81f
Christina Gunarto <cgunarto@gmail.com>
2014-11-05 14:51:01 -0800

    Initial Commit

---
+++ Spa Hunter Challenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Spa Hunter Challenge
+//
+//  Created by CHRISTINA GUNARTO on 11/5/14.
+//  Copyright (c) 2014 Christina Gunarto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
